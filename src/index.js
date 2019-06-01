6171de321d2891a0d3de22a609ef4a4c6d24692a
kellywang1314 <1453501019@qq.com>
2019-06-01 13:09:46 +0800

    docs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
 const App = () => {
-  return <h1>Hello World!</h1>;
-};
+  return <h1>Hello World!</h1>
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
